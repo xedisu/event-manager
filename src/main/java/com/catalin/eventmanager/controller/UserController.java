5c3519cb183ce8e2fe87a7b202ee254cfc118552
Catalin <mihai.c.dobrescu@gmail.com>
2025-03-17 20:45:59 +0200

    Strive to fix tests

--- src/main/java/com/catalin/eventmanager/controller/UserController.java
+++ src/main/java/com/catalin/eventmanager/controller/UserController.java
@@ -1,22 +1,18 @@
 package com.catalin.eventmanager.controller;
 
-import com.catalin.eventmanager.dto.request.UserInformationRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
-    @GetMapping("/user/info")
-    public ResponseEntity<String> getInfo(@RequestParam(required = false) String username) {
-        if (username == null || username.isEmpty()) {
-            return new ResponseEntity<>("Invalid body", HttpStatus.BAD_REQUEST);
-        }
+    @GetMapping("/info")
+    public ResponseEntity<String> getInfo(@RequestParam String username) {
         return new ResponseEntity<>("User info", HttpStatus.OK);
     }
 }
-
--- src/test/java/com/catalin/eventmanager/controller/UserControllerIntegrationTest.java
+++ src/test/java/com/catalin/eventmanager/controller/UserControllerIntegrationTest.java
@@ -1,97 +1,112 @@
 package com.catalin.eventmanager.controller;
 
-import com.catalin.eventmanager.TestConfig;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
 import com.catalin.eventmanager.config.JwtFilter;
-import com.catalin.eventmanager.dto.request.UserInformationRequest;
+import com.catalin.eventmanager.config.SecurityConfig;
 import com.catalin.eventmanager.utils.JwtUtils;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
+
 import jakarta.servlet.http.HttpServletResponse;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import java.util.List;
 
 @WebMvcTest(UserController.class)
-@Import(TestConfig.class)
+@Import(SecurityConfig.class)
 public class UserControllerIntegrationTest {
-
-    @Mock
-    private JwtFilter jwtFilter;
-
-    @MockitoBean
-    private UserDetailsService userDetailsService;
-
-    @Autowired
-    JwtUtils jwtUtils;
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private WebApplicationContext context;
-
-    @BeforeEach
-    void setUp() throws ServletException, IOException {
-        MockitoAnnotations.openMocks(this);
-        doNothing().when(jwtFilter).doFilterInternal(any(HttpServletRequest.class), any(HttpServletResponse.class), any(FilterChain.class));
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
-                .addFilters(jwtFilter)
-                .build();
-    }
+    @MockitoBean JwtUtils jwtUtils;
+    @InjectMocks private JwtFilter jwtFilter;
+    @MockitoBean private UserDetailsService userDetailsService;
+    @Autowired private MockMvc mockMvc;
 
     @Test
     @DisplayName("When /user/info is called, expect 200 code")
     void test() throws Exception {
-        HttpServletResponse httpServletResponse = mockMvc.perform(get("/user/info")).andReturn().getResponse();
+        UserDetails userDetails =
+                new org.springframework.security.core.userdetails.User(
+                        "user", "pula", List.of(new SimpleGrantedAuthority("ROLE_USER")));
+
+        String token = "test-token";
+        when(userDetailsService.loadUserByUsername("user")).thenReturn(userDetails);
+
+        when(jwtUtils.validateToken(token)).thenReturn(true);
+        when(jwtUtils.extractUsername(token)).thenReturn("user");
+        when(jwtUtils.extractAuthorities(token))
+                .thenReturn(List.of(new SimpleGrantedAuthority("ROLE_USER")));
+
+        HttpServletResponse httpServletResponse =
+                mockMvc.perform(
+                                get("/user/info")
+                                        .param("username", "user")
+                                        .header("Authorization", "Bearer " + token))
+                        .andReturn()
+                        .getResponse();
 
         assertEquals(HttpStatus.OK.value(), httpServletResponse.getStatus());
     }
 
     @Test
-    @DisplayName("When /user/info is called with invalid JWTToken, expect 401 code")
-    void testInvalidJWT() throws Exception {
-        doAnswer(invocation -> {
-            HttpServletResponse response = invocation.getArgument(1);
-            response.setStatus(HttpStatus.UNAUTHORIZED.value());
-            return null;
-        }).when(jwtFilter).doFilterInternal(any(HttpServletRequest.class), any(HttpServletResponse.class), any(FilterChain.class));
+    @DisplayName("When /user/info is called with no JWTToken, expect 403 code")
+    void test2() throws Exception {
+        HttpServletResponse httpServletResponse =
+                mockMvc.perform(get("/user/info").param("username", "user2"))
+                        .andReturn()
+                        .getResponse();
 
-        HttpServletResponse httpServletResponse = mockMvc.perform(get("/user/info")).andReturn().getResponse();
-        assertEquals(HttpStatus.UNAUTHORIZED.value(), httpServletResponse.getStatus());
+        assertEquals(HttpStatus.FORBIDDEN.value(), httpServletResponse.getStatus());
     }
 
+    @Test
+    @DisplayName("When /user/info is called with expired JWTToken, expect 403 code")
+    void test3() throws Exception {
+        String token = jwtUtils.generateToken("user", "ROLE_USER");
+        when(jwtUtils.validateToken(token)).thenReturn(false);
+
+        HttpServletResponse httpServletResponse =
+                mockMvc.perform(
+                                get("/user/info")
+                                        .param("username", "user3")
+                                        .header("Authorization", "Bearer " + token))
+                        .andReturn()
+                        .getResponse();
+
+        assertEquals(HttpStatus.FORBIDDEN.value(), httpServletResponse.getStatus());
+    }
 
     @Test
     @DisplayName("When /user/info is called with an invalid body, expect 400 code")
-    void test3() throws Exception {
-        String token = jwtUtils.generateToken("testUsername", "testRole");
+    void test4() throws Exception {
+        UserDetails userDetails =
+                new org.springframework.security.core.userdetails.User(
+                        "user", "pula", List.of(new SimpleGrantedAuthority("ROLE_USER")));
 
-        RequestBuilder request = get("/user/info").header("Authorization", "Bearer " + token)
-                .param("username", "");
+        String token = "test-token";
+        when(userDetailsService.loadUserByUsername("user")).thenReturn(userDetails);
 
-        HttpServletResponse httpServletResponse = mockMvc.perform(request).andReturn().getResponse();
+        when(jwtUtils.validateToken(token)).thenReturn(true);
+        when(jwtUtils.extractUsername(token)).thenReturn("user");
+        when(jwtUtils.extractAuthorities(token))
+                .thenReturn(List.of(new SimpleGrantedAuthority("ROLE_USER")));
+
+        HttpServletResponse httpServletResponse =
+                mockMvc.perform(get("/user/info").header("Authorization", "Bearer " + token))
+                        .andReturn()
+                        .getResponse();
+
         assertEquals(HttpStatus.BAD_REQUEST.value(), httpServletResponse.getStatus());
     }
 }
